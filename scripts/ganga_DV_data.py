aa84d6496f73163b86aa2485005ffc3785e94cd2
Konstantin Gizdov <kgizdov@gmail.com>
2016-03-25 16:55:59 +0000

    put data on DaVinci v35 as well

--- scripts/ganga_DV_data.py
+++ scripts/ganga_DV_data.py
@@ -14,11 +14,13 @@ if mode == 'b2': middle = '_B2psiomega_'
 script='/afs/cern.ch/work/k/kgizdov/Git/jpsi_omega/python/DV' + middle + year + end  # fix path to script
 
 job_name = 'B2psiomega_data' + str(year)
-print job_name
-print script
+print ('Job Name: ' + str(job_name))
+print ('Script: ' + script)
 
 DV = DaVinci()
-DV.version = 'v36r7p7'  # latest is v38r1p1, was set to v36r3p1, working is v36r7p7
+# DV.version = 'v36r7p7'  # latest is v38r1p1, was set to v36r3p1, working is v36r7p7
+DV.version = 'v35r0'  # latest is v38r1p1, was v36r1, working now is v36r7p7
+DV.platform = 'x86_64-slc6-gcc48-opt' # necessary for the older version of DaVinci
 DV.optsfile = [File(script)]
 
 
@@ -47,6 +49,12 @@ for path in BK_locations:
 
 print data
 
+debug = 1
+max_files = -1
+if debug == 1:
+    max_files = 2
+print ('Max Files =' + str(max_files))
+
 import sys
 if len(data.files) < 1:
     sys.exit()
@@ -54,13 +62,13 @@ if len(data.files) < 1:
 j = Job(
     name           = job_name,
     application    = DV,
-    splitter       = SplitByFiles(filesPerJob = 2, maxFiles = -1), # set maxFiles = 1 when debugging and -1 otherwise
+    splitter       = SplitByFiles(filesPerJob = 2, maxFiles = max_files), # set maxFiles = 1 when debugging and -1 otherwise
     inputdata      = data,
     outputfiles     = [LocalFile("*.root")],
     do_auto_resubmit = True,
     backend        = Dirac(),  # Local() for quick debugging, Dirac() for online
     # postprocessors = [RootMerger( files = ['DVTuples1.root'], ignorefailed = True )]
-    postprocessors = [RootMerger( files = ['DVTuples1.root'], ignorefailed = True, overwrite = True )]
+    # postprocessors = [RootMerger( files = ['DVTuples1.root'], ignorefailed = True, overwrite = True )]
     # postprocessors = [SmartMerger( files = ['DVTuples1.root'], ignorefailed = True )]
     )
 j.submit()
