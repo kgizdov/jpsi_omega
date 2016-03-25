517ab95c17765b8ae6ed02cf0753536b26d3da92
Konstantin Gizdov <kgizdov@gmail.com>
2016-03-25 15:16:38 +0000

    updated MC files

--- scripts/ganga_DV_MC.py
+++ scripts/ganga_DV_MC.py
@@ -12,7 +12,7 @@ year = sys.argv[1]
 mode = sys.argv[2]
 magnet = sys.argv[3]
 
-if mode not in ['norm', 'signal', 'background_Bd', 'Bu_JpsiX0', 'Bd_JpsiX0', 'Bs_JpsiX0', 'Lb_JpsiX', 'b2']: sys.exit()
+if mode not in ['Bd_JpsiOm', 'norm', 'signal', 'background_Bd', 'Bu_JpsiX', 'Bd_JpsiX', 'Bs_JpsiX', 'Lb_JpsiX', 'b2']: sys.exit()
 if magnet not in ['MagDown', 'MagUp']: sys.exit()
 
 if mode == 'norm':
@@ -27,10 +27,10 @@ if magnet[3] == 'D':
 if magnet[3] == 'U':
     job_name = job_name + 'MU'
 
-print job_name
-print script
+print ('Job Name: ' + job_name)
+print ('Script: ' + script)
 
-stripping_1 = '/afs/cern.ch/work/k/kgizdov/Git/jpsi_omega/python/stripping_lib/StrippingPsiX0.py'
+# stripping_1 = '/afs/cern.ch/work/k/kgizdov/Git/jpsi_omega/python/stripping_lib/StrippingPsiX0.py'
 
 DV = DaVinci()
 # DV.version = 'v36r7p7'  # latest is v38r1p1, was v36r1, working now is v36r7p7
@@ -39,6 +39,40 @@ DV.platform = 'x86_64-slc6-gcc48-opt' # necessary for the older version of DaVin
 DV.optsfile = [File(script)]
 
 BK_locations = []
+
+## 2011 ##
+
+if year == '2011' and mode == 'Bd_JpsiOm':
+    BK_locations = [
+        '/MC/2011/Beam3500GeV-2011-' + magnet + '-Nu2-Pythia8/Sim08h/Digi13/Trig0x40760037/Reco14c/Stripping20r1NoPrescalingFlagged/11140400/ALLSTREAMS.DST'
+        , '/MC/2011/Beam3500GeV-2011-' + magnet + '-Nu2-EmNoCuts/Sim05/Trig0x40760037Flagged/Reco12a/Stripping17NoPrescalingFlagged/11140400/ALLSTREAMS.DST'
+        # ,'/MC/2011/Beam3500GeV-2011-' + magnet + '-Nu2-Pythia8/Sim08h/Digi13/Trig0x40760037/Reco14c/Stripping20r1NoPrescalingFlagged/11140400/ALLSTREAMS.DST'
+    ]
+
+if year == '2011' and mode == 'Bd_JpsiX':
+    BK_locations = [
+        '/MC/2011/Beam3500GeV-2011-' + magnet + '-Nu2-Pythia6/Sim08c/Digi13/Trig0x40760037/Reco14a/Stripping20r1NoPrescalingFlagged/11442001/ALLSTREAMS.DST'
+        ,'/MC/2011/Beam3500GeV-2011-' + magnet + '-Nu2-Pythia8/Sim08c/Digi13/Trig0x40760037/Reco14a/Stripping20r1NoPrescalingFlagged/11442001/ALLSTREAMS.DST'
+    ]
+
+if year == '2011' and mode == 'Bu_JpsiX':
+    BK_locations = [
+        '/MC/2011/Beam3500GeV-2011-' + magnet + '-Nu2-Pythia6/Sim08c/Digi13/Trig0x40760037/Reco14a/Stripping20r1NoPrescalingFlagged/12442001/ALLSTREAMS.DST'
+        ,'/MC/2011/Beam3500GeV-2011-' + magnet + '-Nu2-Pythia8/Sim08c/Digi13/Trig0x40760037/Reco14a/Stripping20r1NoPrescalingFlagged/12442001/ALLSTREAMS.DST'
+    ]
+
+if year == '2011' and mode == 'Bs_JpsiX':
+    BK_locations = [
+        '/MC/2011/Beam3500GeV-2011-' + magnet + '-Nu2-Pythia6/Sim08c/Digi13/Trig0x40760037/Reco14a/Stripping20r1NoPrescalingFlagged/13442001/ALLSTREAMS.DST'
+        ,'/MC/2011/Beam3500GeV-2011-' + magnet + '-Nu2-Pythia8/Sim08c/Digi13/Trig0x40760037/Reco14a/Stripping20r1NoPrescalingFlagged/13442001/ALLSTREAMS.DST'
+    ]
+
+if year == '2011' and mode == 'Lb_JpsiX':
+    BK_locations = [
+        '/MC/2011/Beam3500GeV-2011-' + magnet + '-Nu2-EmNoCuts/Sim05/Trig0x40760037Flagged/Reco12a/Stripping17NoPrescalingFlagged/15442001/ALLSTREAMS.DST'
+    ]
+
+##########
 
 if year == '2011' and mode == 'background_Bd':
         # Bd -> chic(1,2)K* reflection
@@ -76,19 +110,28 @@ if year == '2012' and mode =='norm':
     BK_locations = [
     ]
 
-if year == '2012' and mode == 'Bu_JpsiX0':
+### 2012 ###
+
+if year == '2012' and mode == 'Bd_JpsiOm':
     BK_locations = [
-        '/MC/2012/Beam4000GeV-2012-' + magnet + '-Nu2.5-Pythia6/Sim08a/Digi13/Trig0x409f0045/Reco14a/Stripping20NoPrescalingFlagged/12442001/ALLSTREAMS.DST'
-        ,'/MC/2012/Beam4000GeV-2012-' + magnet + '-Nu2.5-Pythia8/Sim08a/Digi13/Trig0x409f0045/Reco14a/Stripping20NoPrescalingFlagged/12442001/ALLSTREAMS.DST'
+        '/MC/2012/Beam4000GeV-2012-' + magnet + '-Nu2.5-Pythia8/Sim08h/Digi13/Trig0x409f0045/Reco14c/Stripping20NoPrescalingFlagged/11140400/ALLSTREAMS.DST'
+        # '/MC/2012/Beam4000GeV-2012-' + magnet + '-Nu2.5-Pythia6/Sim08a/Digi13/Trig0x409f0045/Reco14a/Stripping20NoPrescalingFlagged/13442001/ALLSTREAMS.DST'
+        # ,'/MC/2012/Beam4000GeV-2012-' + magnet + '-Nu2.5-Pythia8/Sim08a/Digi13/Trig0x409f0045/Reco14a/Stripping20NoPrescalingFlagged/13442001/ALLSTREAMS.DST'
     ]
 
-if year == '2012' and mode == 'Bd_JpsiX0':
+if year == '2012' and mode == 'Bd_JpsiX':
     BK_locations = [
         '/MC/2012/Beam4000GeV-2012-' + magnet + '-Nu2.5-Pythia6/Sim08a/Digi13/Trig0x409f0045/Reco14a/Stripping20NoPrescalingFlagged/11442001/ALLSTREAMS.DST'
         ,'/MC/2012/Beam4000GeV-2012-' + magnet + '-Nu2.5-Pythia8/Sim08a/Digi13/Trig0x409f0045/Reco14a/Stripping20NoPrescalingFlagged/11442001/ALLSTREAMS.DST'
     ]
 
-if year == '2012' and mode == 'Bs_JpsiX0':
+if year == '2012' and mode == 'Bu_JpsiX':
+    BK_locations = [
+        '/MC/2012/Beam4000GeV-2012-' + magnet + '-Nu2.5-Pythia6/Sim08a/Digi13/Trig0x409f0045/Reco14a/Stripping20NoPrescalingFlagged/12442001/ALLSTREAMS.DST'
+        ,'/MC/2012/Beam4000GeV-2012-' + magnet + '-Nu2.5-Pythia8/Sim08a/Digi13/Trig0x409f0045/Reco14a/Stripping20NoPrescalingFlagged/12442001/ALLSTREAMS.DST'
+    ]
+
+if year == '2012' and mode == 'Bs_JpsiX':
     BK_locations = [
         '/MC/2012/Beam4000GeV-2012-' + magnet + '-Nu2.5-Pythia6/Sim08a/Digi13/Trig0x409f0045/Reco14a/Stripping20NoPrescalingFlagged/13442001/ALLSTREAMS.DST'
         ,'/MC/2012/Beam4000GeV-2012-' + magnet + '-Nu2.5-Pythia8/Sim08a/Digi13/Trig0x409f0045/Reco14a/Stripping20NoPrescalingFlagged/13442001/ALLSTREAMS.DST'
@@ -100,19 +143,7 @@ if year == '2012' and mode == 'b2':
         ,'/MC/2012/Beam4000GeV-2012-' + magnet + '-Nu2.5-Pythia8/Sim08a/Digi13/Trig0x409f0045/Reco14a/Stripping20NoPrescalingFlagged/13442001/ALLSTREAMS.DST'
     ]
 
-if year == '2012' and mode == 'bd':
-    BK_locations = [
-        '/MC/2012/Beam4000GeV-2012-' + magnet + '-Nu2.5-Pythia8/Sim08h/Digi13/Trig0x409f0045/Reco14c/Stripping20NoPrescalingFlagged/11140400/ALLSTREAMS.DST'
-        # '/MC/2012/Beam4000GeV-2012-' + magnet + '-Nu2.5-Pythia6/Sim08a/Digi13/Trig0x409f0045/Reco14a/Stripping20NoPrescalingFlagged/13442001/ALLSTREAMS.DST'
-        # ,'/MC/2012/Beam4000GeV-2012-' + magnet + '-Nu2.5-Pythia8/Sim08a/Digi13/Trig0x409f0045/Reco14a/Stripping20NoPrescalingFlagged/13442001/ALLSTREAMS.DST'
-    ]
-
-if year == '2011' and mode == 'Lb_JpsiX':
-    BK_locations = [
-        '/MC/2011/Beam3500GeV-2011-' + magnet + '-Nu2-EmNoCuts/Sim05/Trig0x40760037Flagged/Reco12a/Stripping17NoPrescalingFlagged/15442001/ALLSTREAMS.DST'
-    ]
-
-
+############
 
 data = LHCbDataset()
 bk = BKQuery()
@@ -129,9 +160,9 @@ if len(data.files) < 1:
     sys.exit()
 
 max_files = -1
-if year == 2012:
-    max_files = 2
-print max_files
+# if year == 2012:
+#     max_files = 2
+print ('Max Files =' + max_files)
 
 j = Job(
     name           = job_name,
