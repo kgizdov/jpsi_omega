9d2a555bf34bf6c03cf0e76b5bb66b4d4df13871
Konstantin Gizdov <kgizdov@gmail.com>
2016-03-29 15:53:52 +0100

    lets see

--- python/DV_B2psiomega_2011_MC_21.py
+++ python/DV_B2psiomega_2011_MC_21.py
@@ -249,7 +249,7 @@ from DSTWriters.Configuration import (SelDSTWriter,
 
 from StrippingSelections.StrippingBandQ import StrippingPsiX0
 
-stripping='stripping21r1'
+stripping='StrippingBandQ'
 config  = strippingConfiguration(stripping)
 archive = strippingArchive(stripping)
 streams = buildStreams(stripping=config, archive=archive)
